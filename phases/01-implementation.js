7876d3304545e91d31d3ab4a6a8751812f20c220
s0618345 <65372382+s0618345@users.noreply.github.com>
2022-06-09 16:35:05 -0400

    a bunch of stuff done two left

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0
+    this.capacity = numBuckets
+    this.data = new Array(this.capacity).fill(null)
   }
 
   hash(key) {
@@ -30,22 +31,82 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    let index = this.hashMod(key)
+    let currentPair = this.data[index]
+    while(currentPair  && currentPair.key != key){
+      currentPair = currentPair.next
+    }
+
+    if (currentPair){
+      currentPair.value = value
+    } else {
+      const newPair = new KeyValuePair(key, value)
+      if (!this.data[index]){
+        this.data[index] = newPair
+      }
+      else {
+        newPair.next = this.data[index]
+        this.data[index] = newPair
+
+
+      }
+      this.count ++
+    }
+
+
+
+
+
   }
 
 
+
   read(key) {
-    // Your code here
+    let index = this.hashMod(key)
+    if (!this.data[index]){return undefined}
+    else{
+      let currentPair = this.data[index]
+      while(currentPair){
+        if (currentPair.key === key){return currentPair.value}
+        currentPair = currentPair.next
+      }
+    }
+return undefined
+
   }
 
 
   resize() {
-    // Your code here
+    let copy = new HashTable(this.capacity)
+    copy.data = this.data
+    this.capacity = this.capacity * 2
+    this.data = new Array(this.capacity).fill(null)
+    this.count = 0
+
+    for (let i = 0; i < copy.data.length; i ++){
+      console.log(copy.data[i])
+      if (!copy.data[i]){continue}
+      if (copy.data[i].next === null){
+        this.insert(copy.data[i].key, copy.data[i].value)
+
+      }
+      else {
+      let currentPair = copy.data[i]
+      while(currentPair){
+        this.insert(currentPair.key, currentPair.value)
+        currentPair = currentPair.next
+
+      }}
+
+
+    }
   }
 
 
   delete(key) {
-    // Your code here
+
+
+
   }
 }
 
