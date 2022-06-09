eeb4ccc6c1d0318382e0f3b5ad33784f4399ea31
s0618345 <65372382+s0618345@users.noreply.github.com>
2022-06-09 18:54:20 -0400

    problem stuck

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -16,20 +16,53 @@ function anagrams(str1, str2) {
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  let newArray = []
+  let set = new Set()
+  for (let i = 0; i < arr1.length; i ++){
+    set.add(arr1[i])
+  }
+  for (let i = 0; i < arr2.length; i ++){
+    if (set.has(arr2[i])){
+      newArray.push(arr2[i])
+    }
+  }
+  return newArray
 }
 
 
 function duplicate(arr) {
-  // Your code here
+  let set = new Set()
+
+  for (let i = 0; i < arr.length; i ++){
+    let size1 = set.size
+    set.add(arr[i])
+    let size2 = set.size
+    if (size1 === size2){return arr[i]}
+  }
 }
 
 
 function twoSum(nums, target) {
-  // Your code here
+let set = new Set()
+for (let i = 0; i < nums.length; i ++){
+  set.add(nums[i])
+}
+
+for (let i = 0; i < nums.length; i ++){
+// let iterator = set.entries()
+//   for (let banana in iterator){
+//     if ((set[0] + nums[i] === target) && set[0] !== nums[i]){return true}
+//   }
+for (let i = 0; i<nums.length; i++){
+  if (set.has(target-nums[i]) && (nums[i] !== target-nums[i])){
+    return true
+  }
 }
 
 
+}
+return false
+}
 function wordPattern(pattern, strings) {
   // Your code here
 }
